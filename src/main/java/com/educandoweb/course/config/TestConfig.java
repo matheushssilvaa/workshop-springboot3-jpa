a09ead8127e9089e330d4b97fb2c010e37ec0fc9
matheushssilvaa <msxatendimento@outlook.com.br>
2025-01-25 08:59:25 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.repositories.OrderItemRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,6 +49,10 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Usuario1", "usuario1@gmail.com", "9888888", "123456");
 		User user2 = new User(null, "Usuario2", "usuario2@gmail.com", "9888888", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+		Order o3 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user1);
+		
 		Product p1 = new Product(null, "Celular Samsung", "consectetur", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
@@ -53,6 +62,11 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
 		p1.getCategories().add(categoria2); // associação da categoria 2 para o produto 1
 		p2.getCategories().add(categoria1);
 		p2.getCategories().add(categoria3);
@@ -61,12 +75,8 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(categoria2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-	
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
-		Order o3 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user1);
-		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
